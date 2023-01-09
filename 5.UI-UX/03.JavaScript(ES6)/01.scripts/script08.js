d06f079b4117c4f55aa92cea5cc227b51d7c9bcc
Aripaka Vamsy Kiran <a.vamc.it@gmail.com>
2023-01-09 17:05:16 +0530

    w7 d1

--- 5.UI-UX/03.JavaScript(ES6)/01.scripts/script08.js
+++ 5.UI-UX/03.JavaScript(ES6)/01.scripts/script08.js
@@ -17,3 +17,35 @@ let emp3 = new Emp(103,"Bhuvana",78000);
 console.log(emp2);
 console.log(emp3);
 
+//Classes
+class Employee {
+    constructor(empId,name,sal){
+        this.empId=empId;
+        this.name=name;
+        this.sal=sal;
+    }
+
+    ta(){
+        return this.sal*0.05;
+    }
+}
+
+let emp4 = new Employee(104,"Anand",78000);
+console.log(emp4);
+console.log(emp4.ta());
+
+//Inheretence
+class Manager extends Employee{
+    constructor(empId,name,sal,allowence){
+        super(empId,name,sal);
+        this.allowence=allowence;
+    }
+
+    ta(){
+        return this.sal*0.15;
+    }   
+}
+
+let emp5 = new Employee(105,"Lakshmi",90000);
+console.log(emp5);
+console.log(emp5.ta());
